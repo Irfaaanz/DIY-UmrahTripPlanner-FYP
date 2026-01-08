f0b1d0bf2d33d806ebd1df89adf9515b3fc73e4b
ianns <fanzr24@gmail.com>
2026-01-08 15:22:21 +0800

    adding some assets and firebase google api files

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:diy_umrah_planner_app/main.dart';
+import 'package:diy_umrah_buddy/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
